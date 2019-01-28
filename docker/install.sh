de9d59b1fb5bde76b6644ac71272eb5cd4cb82bd
Евгений Кривоносов <e.krivonosov@digital-sector.ru>
2019-01-28 13:57:48 +0300

    readme update

--- docker/install.sh
+++ docker/install.sh
@@ -1,7 +1,7 @@
 if [[ ! -d ./.env ]]; then
     cp ./example.env .env
 fi
-folders="logs/nginx logs/php logs/mysql logs/memcached data/sessions data/mysql data/memcached data/elasticsearch data/jenkins_home data/redis ../tests"
+folders="logs/nginx logs/php logs/mysql logs/memcached data/sessions data/mysql data/memcached data/elasticsearch data/jenkins_home data/redis"
 for var in $folders
 do
     if [[ ! -d "./$var" ]]; then
